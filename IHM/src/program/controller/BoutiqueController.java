3b6f4946b4aac4db3258cb4c1ac795aebda55add
FannyKalinowski <kalinowski.fanny@unice.fr>
2019-02-27 17:06:39 +0100

    bisous

--- IHM/src/program/controller/BoutiqueController.java
+++ IHM/src/program/controller/BoutiqueController.java
@@ -1,6 +1,14 @@
 package program.controller;
 
-public class BoutiqueController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import program.View;
+
+import java.io.IOException;
+
+public class BoutiqueController extends HomeController {
 
     public void initialize(){}
+
+
 }
