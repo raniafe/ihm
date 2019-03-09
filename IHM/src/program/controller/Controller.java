8f8ee0a91220562f8e716633e3ea7ccd5c94ab83
FannyKalinowski <kalinowski.fanny@unice.fr>
2019-03-09 11:38:04 +0100

    HomeController + StockController

--- IHM/src/program/controller/Controller.java
+++ IHM/src/program/controller/Controller.java
@@ -1,10 +1,16 @@
 package program.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import program.View;
+import program.model.ModelListOfProduit;
 import program.model.ProduitModel;
 
 import java.io.IOException;
@@ -35,6 +41,37 @@ public  abstract class Controller {
             case "produit": ((ProduitReservationController) loader.getController()).setProduit(produit);
         }
         stage.show();
+    }
+
+
+    public void loadList(ObservableList<ProduitModel> listToObserve, ListView produitListView) {
+
+        ObservableList observableList = FXCollections.observableArrayList();
+        observableList.setAll(listToObserve);
+        produitListView.setItems(observableList);
+        produitListView.setCellFactory((Callback<javafx.scene.control.ListView<String>, ListCell>) listView -> new ListCell<ProduitModel>() {
+            @Override
+            public void updateItem(ProduitModel product, boolean empty) {
+                super.updateItem(product, empty);
+                if (product != null) {
+                    Parent listElement = null;
+                    // Load fxml file for this Person
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("../../resources/fxml/Produit.fxml"));
+
+                    ProduitController produitController = new ProduitController();
+                    loader.setController(produitController);
+                    try {
+                        listElement = loader.load(getClass().getResourceAsStream("../../resources/fxml/Produit.fxml"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    produitController.setInfo(product);
+                    setGraphic(listElement);
+
+                }
+            }
+        });
+
     }
 
 
--- IHM/src/program/controller/StockController.java
+++ IHM/src/program/controller/StockController.java
@@ -16,8 +16,7 @@ import java.io.IOException;
 
 public class StockController extends HomeController {
 
-    private ModelListOfProduit modelListCustomers = null;
-    private ObservableList observableList = FXCollections.observableArrayList();
+    private ModelListOfProduit modelListStock = null;
 
     @FXML
     private Button fapButton;
@@ -25,32 +24,9 @@ public class StockController extends HomeController {
     @FXML
     private ListView produitsListView;
 
-    public void initialize(ModelListOfProduit customersList) {
-        this.modelListCustomers = customersList;
-        observableList.setAll(customersList.getListOfProduitsStock());
-        produitsListView.setItems(observableList);
-        produitsListView.setCellFactory((Callback<ListView<String>, ListCell>) listView -> new ListCell<ProduitModel>() {
-            @Override
-            public void updateItem(ProduitModel product, boolean empty) {
-                super.updateItem(product, empty);
-                if (product != null) {
-                    Parent listElement=null;
-                    // Load fxml file for this Person
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("../../resources/fxml/Produit.fxml"));
-
-                    ProduitController produitController = new ProduitController();
-                    loader.setController(produitController);
-                    try {
-                        listElement = loader.load(getClass().getResourceAsStream("../../resources/fxml/Produit.fxml"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    produitController.setInfo(product);
-                    setGraphic(listElement);
-
-                }
-            }
-        });
+    public void initialize(ModelListOfProduit listOfProduitStock) {
+        this.modelListStock = listOfProduitStock;
+        loadList(modelListStock.getListOfProduitsBoutique(), produitsListView);
         //fapButton.setOnAction(event -> displayFAP());
         //Fonction à déclarer avec les autres Display() ;je ne l'ai pas fait car qqn travaillait dessus
     }
