2e657ba58f4c07b71b7a2dcf636479ceb3fe762f
Lasseron Leo <35364840+LasseronLeo@users.noreply.github.com>
2019-04-29 23:45:36 +0200

    Ajout de la carte
    ajout du module map sans gps

--- GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/fragments/MapFragment.java
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/fragments/MapFragment.java
@@ -8,16 +8,60 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gaspimiamva.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap mGoogleMap;
+    MapView mMapView;
     View myView;
 
-    @Nullable
+    public MapFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_map, container, false);
         return myView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+        mMapView = (MapView) myView.findViewById(R.id.map);
+        if (mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getActivity());
+        mGoogleMap= googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+        LatLng Olives = new LatLng(43.615280, 7.072272);
+        LatLng Pommes = new LatLng(49.079255, -0.655125);
+        googleMap.addMarker(new MarkerOptions().position(Olives).title("1 sac d'olives "));
+        googleMap.addMarker(new MarkerOptions().position(Pommes).title("5kg de pommmes "));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Olives));
     }
 }
