c6b637ef4dceb4235cb0b350893c5b3150abef8a
Fanny-Kalinowski <fanny.kalinowski@etu.unice.fr>
2019-04-29 16:42:13 +0200

    # modele Produit # modele ListProduit

---
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/models/ModelListOfProduit.java
@@ -0,0 +1,20 @@
+package com.example.gaspimiamva.models;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelListOfProduit {
+
+    private static List<Produit> listProduitsStock = new ArrayList<>();
+    private static List<Produit> listProduitsBoutique = new ArrayList<>();
+    private static List<Produit> listProduitReservation = new ArrayList<>();
+    private static List<Produit> listProduitVente = new ArrayList<>();
+
+    public static void init(){
+
+
+
+    }
+}
---
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/models/Produit.java
@@ -0,0 +1,92 @@
+package com.example.gaspimiamva.models;
+
+import java.util.Date;
+
+public class Produit {
+
+    private String name;
+    private int quantite;
+    private String categorie;
+    private Date date ;
+    private String image ;
+    private int prix;
+    private String localisation; // pour les produits de la Boutique
+
+    // constructeur pour la ListStock
+
+    public Produit(String name, int quantite, String categorie, Date date, String image) {
+        this.name = name;
+        this.quantite = quantite;
+        this.categorie = categorie;
+        this.date = date;
+        this.image = image;
+    }
+
+    // constructeur pour la Listvente, ListBoutique, ListReservation
+
+    public Produit(String name, int quantite, String categorie, Date date, String image, int prix, String localisation) {
+        this.name = name;
+        this.quantite = quantite;
+        this.categorie = categorie;
+        this.date = date;
+        this.image = image;
+        this.prix = prix;
+        this.localisation = localisation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+}
