85e965a2805c813e8873cfb88cb1277f085fd46f
boutheinakaroui <boutheina.karoui@etu.unice.fr>
2019-05-10 15:18:10 +0200

    agenda

--- GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/activites/MainActivity.java
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/activites/MainActivity.java
@@ -8,16 +8,18 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,6 +36,11 @@ import com.example.gaspimiamva.fragments.StockFragment;
 import com.example.gaspimiamva.models.ModelListOfProduit;
 import com.example.gaspimiamva.models.UsersListModel;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static com.example.gaspimiamva.R.id.toolbar;
 import java.text.ParseException;
 
 public class MainActivity extends AppCompatActivity
@@ -57,7 +64,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -116,6 +122,7 @@ public class MainActivity extends AppCompatActivity
             */
         }
         //noinspection SimplifiableIfStatement
+
 
         return super.onOptionsItemSelected(item);
     }
