f76adf0d616c722f2f6b2af6ee9aa7683436dc6b
raniafe <raniafekih@hotmail.com>
2019-05-12 18:15:41 +0200

    Agenda + formulaire produit

---
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/adapters/CalendarManager.java
@@ -0,0 +1,58 @@
+package com.example.gaspimiamva.adapters;
+
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+
+import com.example.gaspimiamva.models.Produit;
+
+
+public class CalendarManager {
+    private Activity activity;
+    private ContentResolver cr;
+    private String id;
+
+    public CalendarManager(Activity activity){
+        this.activity = activity;
+        this.cr = activity.getContentResolver();
+        this.id = null;
+    }
+
+    public void init(){
+        String projection[] = {"_id", "calendar_displayName"};
+        Uri calendars;
+        calendars = Uri.parse("content://com.android.calendar/calendars");
+
+        Cursor managedCursor = cr.query(calendars, projection, null, null, null);
+
+        if (managedCursor.moveToFirst()){
+            String calName;
+            int nameCol = managedCursor.getColumnIndex(projection[1]);
+            int idCol = managedCursor.getColumnIndex(projection[0]);
+            do {
+                calName = managedCursor.getString(nameCol);
+                this.id = managedCursor.getString(idCol);
+            } while(managedCursor.moveToNext());
+            managedCursor.close();
+        }
+    }
+
+    public void insert(Produit d){
+        if(this.id != null){
+            ContentValues values = new ContentValues();
+            values.put(CalendarContract.Events.DTSTART, d.getDate().getTime());
+            values.put(CalendarContract.Events.DTEND, d.getDate().getTime());
+            values.put(CalendarContract.Events.TITLE, d.getName());
+            values.put(CalendarContract.Events.DESCRIPTION, d.getDescription());
+            values.put(CalendarContract.Events.CALENDAR_ID, id);
+            values.put(CalendarContract.Events.EVENT_TIMEZONE, "French/Paris");
+
+            //insert event
+            cr.insert(CalendarContract.Events.CONTENT_URI, values);
+        }
+    }
+}
