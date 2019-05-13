8a95b02d396a7675feaad717645d5547775ee7d0
boutheinakaroui <boutheina.karoui@etu.unice.fr>
2019-05-13 11:31:08 +0200

    MODIF

--- GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/fragments/StockFragment.java
+++ GaspiMiamMobile/GaspiMiamVA/app/src/main/java/com/example/gaspimiamva/fragments/StockFragment.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import com.example.gaspimiamva.R;
 import com.example.gaspimiamva.adapters.CustomListView;
 import com.example.gaspimiamva.models.ModelListOfProduit;
+import com.example.gaspimiamva.models.Produit;
 
 
 public class StockFragment extends Fragment {
